87bb20decddaf049087bdd9726014de409c5d9cb
Chris <chris@cypheros.co>
2018-07-07 23:47:08 -0400

    kirin970: Enable vendor property overrides
    Small workaround to display our own strings for the devices model, build
    
    number and security patch level. Since berkeley's vendor props override
    the system ones, we can dictate what specific props the about phone menu
    
    reads from instead.
    
    Requires:
    https://gerrit.cypheros.co/#/c/CypherOS/packages_apps_Settings/+/4451/
    
    Change-Id: Ic56e0e9fc777205fa5c563c69313ea6254b295bf

--- kirin970.mk
+++ kirin970.mk
@@ -69,7 +69,9 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.version.huawei=8.0.0 \
     ro.build.version.release=$(PLATFORM_VERSION) \
     ro.build.version.sdk=$(PLATFORM_SDK_VERSION) \
-    ro.cust.cdrom=/dev/null
+    ro.cust.cdrom=/dev/null \
+	ro.vendor.override.security_patch=$(PLATFORM_SECURITY_PATCH) \
+	ro.vendor.override.build_display=$(BUILD_ID)
 
 # Radio
 PRODUCT_PACKAGES += \
