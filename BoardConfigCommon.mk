8a3468ddfddb0d1fa1f5f75e17762a1c7fc67df6
LuK1337 <priv.luk@gmail.com>
2018-12-08 16:23:58 -0500

    kirin970-common: Update display engine and smart display blobs
    * Also drop DisplayEngineService_V1_0 support while at it.
    
    Change-Id: Ieff7f460f75f148a0e262ca22e7248b86c8f465d

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,8 +77,7 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/public
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
-    /system/lib64/libdisplayenginesvc_1_0.so|libshims_hwsmartdisplay_jni.so \
-    /system/lib64/libdisplayenginesvc_1_1.so|libshims_hwsmartdisplay_jni.so \
+    /system/lib64/libdisplayengineservice.so|libshims_hwsmartdisplay_jni.so \
     /system/lib64/libhwsmartdisplay_jni.so|libshims_hwsmartdisplay_jni.so \
     /vendor/bin/hw/vendor.huawei.hardware.hisupl@1.0-service|libshims_hisupl.so
 
